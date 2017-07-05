834276aaf44337e3c61aa05394dd43e3d60b80d0
Marco Bosio <marco.bosio@axant.it>
2017-07-05 14:43:14 +0200

    Added metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+except IOError:
+    README = ''
+
 
 install_requires=[
     "configparser",
@@ -11,10 +19,28 @@ testpkgs = [
     "coverage",
     "mock"
 ]
-setup(description='ContactHub SDK Python',
+setup(description='This is the official Python SDK for ContactHub REST API. This SDK easily allows to access your data '
+                  'on ContactHub, making the authentication immediate and simplifying read/write operations.',
       author='Axant',
       url='https://github.com/axant/contacthub-sdk-python',
       version='0.1',
+      classifiers=['Intended Audience :: Developers',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.5-dev',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.6-dev',
+                   'Programming Language :: Python :: 3.7-dev',
+                   'Programming Language :: Python :: nightly',
+                   'Topic :: Internet :: WWW/HTTP ',
+                   'Topic :: API :: SDK ',
+                   'Topic :: Software Development :: Libraries :: Python Modules'],
+      keywords='web skd api',
+      author_email='tech@axant.it',
       install_requires=install_requires,
       packages=find_packages(exclude=['tests', 'tests.*']),
       extras_require={
@@ -22,7 +48,8 @@ setup(description='ContactHub SDK Python',
           'documentation': ['Sphinx==1.4.1', 'sphinx_rtd_theme']
       },
       scripts=[],
-      name='contacthub',
+      name='contacthub-sdk-python',
+      long_description=README,
       include_package_data=False,
       zip_safe=False,
       test_suite='nose.collector',
